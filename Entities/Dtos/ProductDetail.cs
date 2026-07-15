3d18554f7e048a0d22b1b00df185aa15e90d5e65
rahmaniftaliev <rahmaniftaliev@gmail.com>
2026-07-15 13:07:46 +0400

    Add project files.

---
+++ Entities/Dtos/ProductDetail.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Dtos
+{
+    public class ProductDetail:IDto
+    {
+        public int ProductID { get; set; }
+        public int ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
